54aef58156327016272e67871628a8a9a0625f00
FredLavoie <flavoie3@gmail.com>
2019-05-16 19:09:44 -0400

    add script functions to make webpage interactive

--- script.js
+++ script.js
@@ -1,18 +1,50 @@
-class City {
-  constructor(num, name, Ss, Sr) {
-    this.num = num;
-    this.name = name;
-    this.Ss = Ss;
-    this.Sr = Sr;
+/* eslint-env browser */
+// list of cities with their data
+const cities = [
+  {name: 'Cornwall', Ss: 2.1, Sr: 0.4},
+  {name: 'Kingston', Ss: 2.1, Sr: 0.4},
+  {name: 'Ottawa', Ss: 2.1, Sr: 0.4},
+];
+
+// list of load factors
+let cb = 0.8;
+let cw = 1;
+let cs = 1;
+let ca = 1;
+
+// get 'Ss' and 'Sr' values in city based on selection
+const city = document.querySelector('#city');
+let snowData = [];
+console.log(snowData);
+city.onchange = function() {
+  for(let i = 0; i < cities.length; i++) {
+    if(cities[i].name == city.value) {
+      document.querySelector('#ss').innerHTML = cities[i].Ss + ' kPa';
+      document.querySelector('#sr').innerHTML = cities[i].Sr + ' kPa';
+      snowData.push(cities[i].Ss);
+      snowData.push(cities[i].Sr);
+      console.log(snowData);
+    }
   }
-}
+};
 
-let Cornwall = new City(1, 'Cornwall', 2.1, 0.4);
-let Kingston = new City(2, 'Kingston', 2.1, 0.4);
-let Ottawa = new City(3, 'Ottawa', 2.1, 0.4);
+// display importance factor based on selected category
+const imp = document.querySelector('#importance');
+let impFactor = [];
+imp.onchange = function() {
+  impFactor.push(Number(imp.value));
+  document.querySelector('#imp').innerHTML = 'Importance factor, Ie = ' + (imp.value);
+  console.log(impFactor);
+};
 
 
-let city = document.getElementById('city').value;
+// calculate and display the basic snow load
+const finalAnswer = document.querySelector('#answer');
+finalAnswer.onclick = function() {
+  let ss = snowData[0];
+  let sr = snowData[1];
+  let ie = impFactor[0];
+  let snow = ie * (ss * (cb * cw * cs * ca) + sr);
+  document.querySelector('#snow').innerHTML = snow + ' kPa';
+};
 
-
-console.log(city);
