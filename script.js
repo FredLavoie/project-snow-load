b49fc59a99f53ac51fe8f0b58ff6047ebce66bd4
FredLavoie <flavoie3@gmail.com>
2019-05-16 21:32:27 -0400

    fix error in JS related to changing dropdown value and update button style

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 /* eslint-env browser */
 // list of cities with their data
 const cities = [
-  {name: 'Cornwall', Ss: 2.1, Sr: 0.4},
+  {name: 'Cornwall', Ss: 2.2, Sr: 0.4},
   {name: 'Kingston', Ss: 2.1, Sr: 0.4},
-  {name: 'Ottawa', Ss: 2.1, Sr: 0.4},
+  {name: 'Ottawa', Ss: 2.4, Sr: 0.4},
 ];
 
 // list of load factors
@@ -14,27 +14,24 @@ let ca = 1;
 
 // get 'Ss' and 'Sr' values in city based on selection
 const city = document.querySelector('#city');
-let snowData = [];
-console.log(snowData);
+let snowData = [0, 0];
 city.onchange = function() {
   for(let i = 0; i < cities.length; i++) {
     if(cities[i].name == city.value) {
       document.querySelector('#ss').innerHTML = cities[i].Ss + ' kPa';
       document.querySelector('#sr').innerHTML = cities[i].Sr + ' kPa';
-      snowData.push(cities[i].Ss);
-      snowData.push(cities[i].Sr);
-      console.log(snowData);
+      snowData[0] = (cities[i].Ss);
+      snowData[1] = (cities[i].Sr);
     }
   }
 };
 
 // display importance factor based on selected category
 const imp = document.querySelector('#importance');
-let impFactor = [];
+let impFactor = [0];
 imp.onchange = function() {
-  impFactor.push(Number(imp.value));
+  impFactor[0] = (Number(imp.value));
   document.querySelector('#imp').innerHTML = 'Importance factor, Ie = ' + (imp.value);
-  console.log(impFactor);
 };
 
 
@@ -45,6 +42,6 @@ finalAnswer.onclick = function() {
   let sr = snowData[1];
   let ie = impFactor[0];
   let snow = ie * (ss * (cb * cw * cs * ca) + sr);
-  document.querySelector('#snow').innerHTML = snow + ' kPa';
+  document.querySelector('#snow').innerHTML = snow.toFixed(2) + ' kPa';
 };
 
